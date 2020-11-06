9ed16b679e143019a73b47f54663dc591b07a827
没咸鱼的梦想 <466375758@qq.com>
2020-11-06 17:29:20 +0800

    Update test_foo.py
    for flake8 detection

--- test_foo.py
+++ test_foo.py
@@ -11,15 +11,16 @@ def test_invalid_input():
 def test_valid_negative():
     pass
 
+
 @patch('functions.get_stuff')
 def test_thirteen(get_stuff):
     get_stuff.return_value = 7
     pass
 
+
 # Mock this method as well
 def test_thirteen_three():
     pass
-
 
 
 def test_valid_input_positive():
