603b8154adb180ea2178571a624d7ef43f23379a
yixianLu <43053645+yixianLu@users.noreply.github.com>
2020-11-06 09:35:35 +0800

    update yml download google drive

--- test_selenium.py
+++ test_selenium.py
@@ -1,3 +1,29 @@
+# """
+# Modify this file
+# """
+# from selenium import webdriver
+# from selenium.webdriver.common.keys import Keys
+
+# """
+# Refer to https://selenium-python.readthedocs.io/locating-elements.html
+# for more information on how to locate elements
+# """
+
+# url = "https://cs.ualberta.ca"
+
+# path = "./chromedriver"
+
+# def test_401_result():
+#     driver = webdriver.Chrome(executable_path=path)
+#     driver.get(url)
+#     #Fill me in
+
+#     driver.quit()
+
+# def test_empty_result():
+#     # Fill me in
+#     pass
+
 """
 Modify this file
 """
@@ -11,10 +37,9 @@ for more information on how to locate elements
 
 url = "https://cs.ualberta.ca"
 
-path = "./chromedriver"
 
 def test_401_result():
-    driver = webdriver.Chrome(executable_path=path)
+    driver = webdriver.Chrome()
     driver.get(url)
     #Fill me in
 
